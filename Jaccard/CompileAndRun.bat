a0c8e66d847437f2e4f7882920539a595e7e6b02
Cjsheaf <cjsheaf@cox.net>
2013-12-05 21:06:58 -0800

    Updated batch files.

--- Jaccard/CompileAndRun.bat
+++ Jaccard/CompileAndRun.bat
@@ -1,8 +1,3 @@
-echo off
-
 gcc -std=c99 -c "C Files\jaccard.c" -o "Object Files\jaccard.o"
 
 gcc -std=c99 "Object Files\jaccard.o" "..\List-Library\Object Files\List.o" -o jaccard
-
-jaccard a.csv b.csv c.csv d.csv
-pause
--- Shingle/CompileAndRun.bat
+++ Shingle/CompileAndRun.bat
@@ -1,8 +1,3 @@
-echo off
-
 gcc -std=c99 -c "C Files\shingle.c" -o "Object Files\shingle.o"
 
 gcc -std=c99 "Object Files\shingle.o" "..\List-Library\Object Files\List.o" -o shingle
-
-shingle -i Input.txt -o Output.csv -s 3 -d
-pause
